2ec67fe73f5aca9ef734ccb056ecb529d498198d
vaishalipawar1490 <vaishalipawar1490@github.com>
2024-10-07 16:10:46 +0530

    Update install-packer

--- images/macos/scripts/build/install-packer.sh
+++ images/macos/scripts/build/install-packer.sh
@@ -1,1 +1,14 @@
+#!/bin/bash -e -o pipefail
+################################################################################
+##  File:  install-bicep.sh
+##  Desc:  Install bicep cli
+################################################################################
+
+source ~/utils/utils.sh
+
+echo "Installing packer..."
+brew tap hashicorp/tap
+brew_smart_install hashicorp/tap/packer
+
+invoke_tests "Common" "Packer"
 
