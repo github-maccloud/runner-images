30b5440ede836dee042c259d65fecc8820326e15
Suresh kumar <v-erkumar@microsoft.com>
2025-03-21 17:58:21 +0530

    change the kotlin code

--- images/macos/scripts/build/install-kotlin.sh
+++ images/macos/scripts/build/install-kotlin.sh
@@ -8,7 +8,12 @@ source ~/utils/utils.sh
 
 echo Installing Kotlin
 kotlinVersionToolset=$(get_toolset_value '.kotlin.version')
-brew_smart_install "kotlin@${kotlinVersionToolset}"
+#brew_smart_install "kotlin@${kotlinVersionToolset}"
+
+brew uninstall kotlin || true
+brew extract --version=${kotlinVersionToolset} kotlin homebrew/cask-versions
+brew install ./kotlin@${kotlinVersionToolset}.rb
+brew link --force kotlin@${kotlinVersionToolset}
 
 echo Verifying Kotlin installation
 kotlinc -version
