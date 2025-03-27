833c858501baa4ae9292bec2417e4c3f97435795
susmitamane <susmitamane@github.com>
2025-03-27 20:18:46 +0530

    Update install-edge.sh

--- images/macos/scripts/build/install-edge.sh
+++ images/macos/scripts/build/install-edge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e -o pipefail
 ################################################################################
 ##  File:  install-edge.sh
-##  Desc:  Install edge browser for both Intel and ARM64 architectures
+##  Desc:  Install edge browser
 ################################################################################
 
 source ~/utils/utils.sh
@@ -17,16 +17,16 @@ echo "Version of Microsoft Edge: ${edge_version}"
 
 echo "Installing Microsoft Edge WebDriver..."
 
-arch_name="$(uname -m)"
-if [[ "$arch_name" == "arm64" ]]; then
-    edge_driver_url="https://msedgedriver.azureedge.net/${edge_driver_latest_version}/edgedriver_mac64_m1.zip"
-else
-    edge_driver_url="https://msedgedriver.azureedge.net/${edge_driver_latest_version}/edgedriver_mac64.zip"
-fi
+edge_driver_version_file_path=$(download_with_retry "https://msedgedriver.azureedge.net/LATEST_RELEASE_${edge_version_major}_MACOS")
+edge_driver_latest_version=$(iconv -f utf-16 -t utf-8 "$edge_driver_version_file_path" | tr -d '\r')
+edge_driver_url="https://msedgedriver.azureedge.net/${edge_driver_latest_version}/edgedriver_mac64.zip"
 
 echo "Compatible version of WebDriver: ${edge_driver_latest_version}"
 
 edge_driver_archive_path=$(download_with_retry "$edge_driver_url")
+
+# Move webdriver to the separate directory to be consistent with the docs
+# https://docs.microsoft.com/en-us/azure/devops/pipelines/test/continuous-test-selenium?view=azure-devops#decide-how-you-will-deploy-and-test-your-app
 
 EDGE_DRIVER_DIR="/usr/local/share/edge_driver"
 mkdir -p $EDGE_DRIVER_DIR
@@ -35,8 +35,14 @@ ln -s $EDGE_DRIVER_DIR/msedgedriver /usr/local/bin/msedgedriver
 
 echo "export EDGEWEBDRIVER=${EDGE_DRIVER_DIR}" >> ${HOME}/.bashrc
 
+# Verify architecture
+installed_arch=$(lipo -info "$EDGE_DRIVER_DIR/msedgedriver")
+echo "Edge WebDriver installed with architectures: $installed_arch"
+
 # Configure Edge Updater to prevent auto update
-sudo mkdir -p "/Library/Managed Preferences"
+# https://learn.microsoft.com/en-us/deployedge/edge-learnmore-edgeupdater-for-macos
+
+sudo mkdir "/Library/Managed Preferences"
 
 cat <<EOF | sudo tee "/Library/Managed Preferences/com.microsoft.EdgeUpdater.plist" > /dev/null
 <?xml version="1.0" encoding="UTF-8"?>
