105c40dc846be8b57bed976b46741429fdeb6cf9
Erik Bershel <110455084+erik-bershel@users.noreply.github.com>
2025-04-16 19:24:58 +0200

    [macOS] Add DeveloperTools permission into TCC for bash and Terminal

--- images/macos/scripts/build/configure-tccdb-macos.sh
+++ images/macos/scripts/build/configure-tccdb-macos.sh
@@ -32,6 +32,10 @@ systemValuesArray=(
     "'kTCCServiceSystemPolicyAllFiles','com.microsoft.wdav.epsext',0,2,4,1,NULL,NULL,NULL,'UNUSED',NULL,0,1643970979"
     "'kTCCServiceSystemPolicyNetworkVolumes','/bin/bash',1,2,0,1,NULL,NULL,NULL,'UNUSED',NULL,0,1583997993"
     "'kTCCServiceSystemPolicyNetworkVolumes','com.apple.Terminal',0,2,4,1,X'fade0c000000003000000001000000060000000200000012636f6d2e6170706c652e5465726d696e616c000000000003',NULL,0,'UNUSED',NULL,0,1678990068"
+    # Sequoia has a new mandatory TCC service for Developer Tools aka Automation on Privacy & Security settings tab
+    # Expected to fix https://github.com/actions/runner-images/issues/11257
+    "'kTCCServiceDeveloperTool','/bin/bash',1,2,0,1,NULL,NULL,NULL,'UNUSED',NULL,0,1583997993"
+    "'kTCCServiceDeveloperTool','com.apple.Terminal',0,2,4,1,X'fade0c000000003000000001000000060000000200000012636f6d2e6170706c652e5465726d696e616c000000000003',NULL,0,'UNUSED',NULL,0,1678990068"
 )
 for values in "${systemValuesArray[@]}"; do
     if is_Sonoma || is_Sequoia; then
