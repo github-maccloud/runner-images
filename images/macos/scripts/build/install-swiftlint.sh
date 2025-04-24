e2e2a8f72300da21ac8bf1e7daa3788040e729a1
sangeeths03 <sangeeths03@github.com>
2025-04-24 14:06:49 +0530

    swiftlint-version-downgrade

--- images/macos/scripts/build/install-swiftlint.sh
+++ images/macos/scripts/build/install-swiftlint.sh
@@ -1,13 +1,20 @@
 #!/bin/bash -e -o pipefail
 ################################################################################
 ##  File:  install-swiftlint.sh
-##  Desc:  Install SwiftLint
+##  Desc:  Install SwiftLint 0.58.2
 ################################################################################
 
 source ~/utils/utils.sh
 
-echo "Installing Swiftlint..."
+echo "Installing SwiftLint 0.58.2..."
 
-brew_smart_install "swiftlint"
+# Pin SwiftLint to 0.58.2 due to issues with newer versions
+# https://github.com/Homebrew/homebrew-core/pull/204527
+swiftlint_commit="f46d29944b415df9f1eedad85e4fde41a948c7c0"
+swiftlint_rb_link="https://raw.githubusercontent.com/Homebrew/homebrew-core/$swiftlint_commit/Formula/s/swiftlint.rb"
+swiftlint_rb_path=$(download_with_retry "$swiftlint_rb_link")
 
+brew install "$swiftlint_rb_path"
+
+# Run the test hook
 invoke_tests "Linters" "SwiftLint"
