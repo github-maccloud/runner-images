21c56599c355b3d8258e847a81ba3e69f2c68b2b
Pavel Iakovenko <paveliak@users.noreply.github.com>
2025-06-26 21:31:31 -0400

    Sprinkle diskutil info

--- images/macos/scripts/build/Install-Xcode.ps1
+++ images/macos/scripts/build/Install-Xcode.ps1
@@ -14,8 +14,10 @@ write-host $xcodeVersions
 $defaultXcode = (Get-ToolsetContent).xcode.default
 [Array]::Reverse($xcodeVersions)
 $threadCount = "5"
-
+    
 Write-Host "Installing Xcode versions..."
+diskutil info /
+
 $xcodeVersions | ForEach-Object -ThrottleLimit $threadCount -Parallel {
     $ErrorActionPreference = "Stop"
     Import-Module "$env:HOME/image-generation/helpers/Common.Helpers.psm1"
@@ -23,19 +25,26 @@ $xcodeVersions | ForEach-Object -ThrottleLimit $threadCount -Parallel {
 
     Install-XcodeVersion -Version $_.version -LinkTo $_.link -Sha256Sum $_.sha256
     Confirm-XcodeIntegrity -Version $_.link
+    diskutil info /
 }
 
 $xcodeVersions | ForEach-Object {
     Approve-XcodeLicense -Version $_.link
 }
 
+diskutil info /
+    
 Write-Host "Configuring Xcode versions..."
 $xcodeVersions | ForEach-Object {
     Write-Host "Configuring Xcode $($_.link) ..."
     Invoke-XcodeRunFirstLaunch -Version $_.link
+    diskutil info /
     Install-AdditionalSimulatorRuntimes -Version $_.link -Arch $arch -Runtimes $_.install_runtimes
+    diskutil info /
 }
 
+diskutil info /
+    
 Invoke-XcodeRunFirstLaunch -Version $defaultXcode
 
 Write-Host "Configuring Xcode symlinks..."
@@ -48,14 +57,21 @@ $xcodeVersions | ForEach-Object {
     }
 }
 
+diskutil info /
+    
 Write-Host "Rebuilding Launch Services database ..."
 $xcodeVersions | ForEach-Object {
     Initialize-XcodeLaunchServicesDb -Version $_.link
 }
 
+diskutil info /
+    
 Write-Host "Setting default Xcode to $defaultXcode"
 Switch-Xcode -Version $defaultXcode
 New-Item -Path "/Applications/Xcode.app" -ItemType SymbolicLink -Value (Get-XcodeRootPath -Version $defaultXcode) | Out-Null
 
 Write-Host "Setting environment variables 'XCODE_<VERSION>_DEVELOPER_DIR'"
 Set-XcodeDeveloperDirEnvironmentVariables -XcodeList $xcodeVersions.link
+
+diskutil info /
+    
