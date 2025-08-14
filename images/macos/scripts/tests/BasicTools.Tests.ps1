6643f56a2202f1d57397decee3bd9885e9eecaa8
Erik Bershel <110455084+erik-bershel@users.noreply.github.com>
2025-08-14 12:29:56 +0200

    Prettify TCL/TK condition

--- images/macos/scripts/tests/BasicTools.Tests.ps1
+++ images/macos/scripts/tests/BasicTools.Tests.ps1
@@ -56,7 +56,7 @@ Describe "Perl" {
     }
 }
 
-Describe "Tcl/Tk" -Skip:($os.IsArm64 -or $os.IsSequoia -or $os.IsTahoe) {
+Describe "Tcl/Tk" -Skip:((-not $os.IsVenturaX64) -or (-not $os.IsSonomaX64)) {
     It "libtcl" {
         Test-Path "/usr/local/lib/libtcl8.6.dylib" | Should -BeTrue
         Test-Path "/usr/local/lib/libtk8.6.dylib" | Should -BeTrue
