1a125b221c64748c561609967b17a2d1492efea2
Shamil Mubarakshin <shamil-mubarakshin@github.com>
2025-10-30 15:11:07 +0100

    Fix pinning

--- images/macos/scripts/build/install-xcbeautify.sh
+++ images/macos/scripts/build/install-xcbeautify.sh
@@ -12,8 +12,11 @@ echo "Installing xcbeautify..."
 if is_Sonoma; then
     tool_commit="ea85fd1ef3aa6d60aabf5313989b3d0d68b48cd2"
     tool_rb_link="https://raw.githubusercontent.com/Homebrew/homebrew-core/$tool_commit/Formula/x/xcbeautify.rb"
-    tool_rb_path=$(download_with_retry "$tool_rb_link")
-    brew install "$tool_rb_path"
+    tool_rb_path="$(brew --repository)/Library/Taps/homebrew/homebrew-core/Formula/x/xcbeautify.rb"
+
+    mkdir -p "$(dirname $tool_rb_path)"
+    curl -fsSL $tool_rb_link -o $tool_rb_path
+    HOMEBREW_NO_AUTO_UPDATE=1 HOMEBREW_NO_INSTALL_FROM_API=1 brew install xcbeautify
 else
     brew_smart_install xcbeautify
 fi
