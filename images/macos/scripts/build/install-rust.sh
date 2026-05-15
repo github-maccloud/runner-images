b394ea4d0cb6cbe93a458a018f2a9a1a50b7344b
v-sergei-pyshnoi <121864472+v-sergei-pyshnoi@users.noreply.github.com>
2026-05-15 13:38:12 +0200

    Implement checksum validation for Rust installation
    Added checksum validation for rustup-init download to enhance supply chain security.

--- images/macos/scripts/build/install-rust.sh
+++ images/macos/scripts/build/install-rust.sh
@@ -2,12 +2,27 @@
 ################################################################################
 ##  File:  install-rust.sh
 ##  Desc:  Install Rust
+##  Supply chain security: rustup-init - checksum validation
 ################################################################################
 
 source ~/utils/utils.sh
 
 echo "Installing Rustup..."
-brew_smart_install "rustup-init"
+if is_Arm64; then
+    arch="aarch64-apple-darwin"
+else 
+    arch="x86_64-apple-darwin"
+fi
+
+download_url="https://static.rust-lang.org/rustup/dist/${arch}/rustup-init"
+external_hash="$(curl -fsSL ${download_url}.sha256 | awk '{print $1}')"
+
+binary_path=$(download_with_retry $download_url)
+
+use_checksum_comparison "$binary_path" "$external_hash"
+
+chmod +x "$binary_path"
+mv "$binary_path" /usr/local/bin/rustup-init
 
 echo "Installing Rust language..."
 rustup-init -y --no-modify-path --default-toolchain=stable --profile=minimal
